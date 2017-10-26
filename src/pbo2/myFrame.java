80e7e064187479084b0be52a457174fd0047e91f
muhammadzaini0190 <33127048+muhammadzaini0190@users.noreply.github.com>
2017-10-26 23:34:16 +0800

    menambahkan"attribute"

--- src/pbo2/myFrame.java
+++ src/pbo2/myFrame.java
@@ -1,9 +1,13 @@
 package pbo2;
-
-/**
- *
- * @author ASUS
- */
 public class myFrame {
+    String pesan ;
+    int bilBulat ;
+    double bilDesimal ;
+    
+    void cetakAttribute(){
+            System.out.println("isi dari pesan :"+pesan);
+            System.out.println("isi dari bilBulat :"+bilBulat);
+            System.out.println("isi dari bilDesimal :"+bilDesimal);
+    
     
 }
